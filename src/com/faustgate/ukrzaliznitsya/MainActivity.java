28b291361d2baeb0ad878afa24aef11770150290
faustgate <faustgate@gmail.com>
2016-11-15 16:28:10 +0200

    1. Added icon 2. Added correct checks for user input 3. Added check if station info was found

--- src/com/faustgate/ukrzaliznitsya/MainActivity.java
+++ src/com/faustgate/ukrzaliznitsya/MainActivity.java
@@ -1,8 +1,13 @@
 package com.faustgate.ukrzaliznitsya;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.*;
 
@@ -17,6 +22,11 @@ public class MainActivity extends Activity {
     private String stationFromName = "";
     private String stationToId = "";
     private String stationToName = "";
+    private boolean isDepStationCorrect = false;
+    private boolean isArrStationCorrect = false;
+    private DelayAutoCompleteTextView stationFromEdit;
+    private DelayAutoCompleteTextView stationToEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +35,8 @@ public class MainActivity extends Activity {
 
         date.add(Calendar.DAY_OF_MONTH, 1);
 
-        DelayAutoCompleteTextView stationFromEdit = (DelayAutoCompleteTextView) findViewById(R.id.stationFrom);
-        DelayAutoCompleteTextView stationToEdit = (DelayAutoCompleteTextView) findViewById(R.id.stationTo);
+        stationFromEdit = (DelayAutoCompleteTextView) findViewById(R.id.stationFrom);
+        stationToEdit = (DelayAutoCompleteTextView) findViewById(R.id.stationTo);
         DatePicker start_date = (DatePicker) findViewById(R.id.datePicker);
         Button search = (Button) findViewById(R.id.button);
 
@@ -43,16 +53,7 @@ public class MainActivity extends Activity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //   if ((!stationFromId.equals("") && !stationToId.equals(""))) {
-                String data = MessageFormat.format("{0} {1} {2} {3} {4}", stationFromName, stationFromId, stationToName,
-                        stationToId, getDateString());
-                Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
-                String trainData = UZRequests.getInstance().searchForTrains(stationFromId, stationToId, date.getTime());
-
-                Intent intent = new Intent(MainActivity.this, TrainListActivity.class);
-                intent.putExtra("trains", trainData);
-                startActivity(intent);
-
+                start();
             }
         });
 
@@ -65,29 +66,186 @@ public class MainActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 HashMap<String, String> station = (HashMap<String, String>) adapterView.getItemAtPosition(position);
-                stationFromName = station.get("title");
-                stationFromId = station.get("station_id");
-                stationFromEdit.setText(stationFromName);
+                initStationFrom(station);
+                adapter.clear();
+                stationFromEdit.setSelection(stationFromEdit.getText().length());
+            }
+        });
+        stationFromEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (!isDepStationCorrect) {
+                        if (adapter.getCount() > 0) {
+                            HashMap<String, String> station = adapter.getItem(0);
+                            initStationFrom(station);
+                            adapter.clear();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Wrong departure station", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    adapter.clear();
+                } else
+                    stationFromEdit.setSelection(stationFromEdit.getText().length());
+            }
+        });
+        stationFromEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                stationToEdit.requestFocus();
+                return true;
+            }
+        });
+        stationFromEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                isDepStationCorrect = false;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
 
+
         stationToEdit.setAdapter(adapter);
-        stationToEdit.setLoadingIndicator((ProgressBar) findViewById(R.id.progress_bar));
+        stationToEdit.setLoadingIndicator((ProgressBar) findViewById(R.id.progress_bar2));
         stationToEdit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 HashMap<String, String> station = (HashMap<String, String>) adapterView.getItemAtPosition(position);
-                stationToName = station.get("title");
-                stationToId = station.get("station_id");
-                stationToEdit.setText(stationToName);
+                initStationTo(station);
+                adapter.clear();
+                stationToEdit.setSelection(stationToEdit.getText().length());
+            }
+        });
+        stationToEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if (!isArrStationCorrect) {
+                        if (adapter.getCount() > 0) {
+                            HashMap<String, String> station = adapter.getItem(0);
+                            initStationTo(station);
+                            adapter.clear();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Wrong arrival station", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    adapter.clear();
+                } else
+                    stationToEdit.setSelection(stationToEdit.getText().length());
             }
         });
 
+        stationToEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (!isArrStationCorrect) {
+                    if (adapter.getCount() > 0) {
+                        HashMap<String, String> station = adapter.getItem(0);
+                        initStationTo(station);
+                        adapter.clear();
+                        isArrStationCorrect = true;
+                    }
+                }
+                if (isDepStationCorrect) {
+                    start();
+                } else {
+                    stationFromEdit.requestFocus();
+                }
+                return true;
+            }
+        });
 
+        stationToEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                isArrStationCorrect = false;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void initStationFrom(HashMap<String, String> station) {
+        stationFromName = station.get("title");
+        stationFromId = station.get("station_id");
+        stationFromEdit.setText(stationFromName);
+        isDepStationCorrect = true;
+    }
+
+    private void initStationTo(HashMap<String, String> station) {
+        stationToName = station.get("title");
+        stationToId = station.get("station_id");
+        stationToEdit.setText(stationToName);
+        isArrStationCorrect = true;
     }
 
     private String getDateString() {
         SimpleDateFormat spf = new SimpleDateFormat("MM.dd.yyyy");
         return spf.format(date.getTime());
     }
+
+    private void start() {
+        if ((!isDepStationCorrect) || stationFromEdit.getText().length() == 0) {
+            new AlertDialog.Builder(MainActivity.this).setTitle("Wrong departure station!")
+                    .setMessage("Departure station wrong or empty")
+                    .setCancelable(false)
+                    .setNegativeButton("ОК",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            }).show();
+            stationFromEdit.requestFocus();
+            return;
+        }
+        if (!isArrStationCorrect || stationToEdit.getText().length() == 0) {
+            new AlertDialog.Builder(MainActivity.this).setTitle("Wrong arrival station!")
+                    .setMessage("Arrival station wrong or empty")
+                    .setCancelable(false)
+                    .setNegativeButton("ОК",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            }).show();
+            stationToEdit.requestFocus();
+            return;
+        }
+        if (stationFromId.equals(stationToId)) {
+            new AlertDialog.Builder(MainActivity.this).setTitle("Same stations")
+                    .setMessage("Arrival and departure stations are the same")
+                    .setCancelable(false)
+                    .setNegativeButton("ОК",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            }).show();
+            stationFromEdit.requestFocus();
+            return;
+        }
+
+        String data = MessageFormat.format("{0} {1} {2} {3} {4}", stationFromName, stationFromId, stationToName,
+                stationToId, getDateString());
+        Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+        String trainData = UZRequests.getInstance().searchForTrains(stationFromId, stationToId, date.getTime());
+
+        Intent intent = new Intent(MainActivity.this, TrainListActivity.class);
+        intent.putExtra("trains", trainData);
+        startActivity(intent);
+    }
+
 }
