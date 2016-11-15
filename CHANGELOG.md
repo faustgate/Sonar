61a256a387a17986b5a46c671659553efce896bc
faustgate <faustgate@gmail.com>
2016-11-15 18:09:12 +0200

    Started main project

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) 
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### Added
+- Added showing prices in tickets selecting
+- Added input First name and last name
+- Added support for booking, bedding, students and child
+- Changed strings from hardcode to string resources
+- Added searching for whole coupe
+- Added ticket watcher
+- Added notification if tickets was found
+
+## [0.0.0-alfa] - 2016-11-11
+### Added
+- Seems like it's alive!!!!! Alfa version  
+- Turn UZRequests into Singleton
+- Implemented Activity for tickets buying via WebView 
+- Implemented tickets order
+- Implemented Activity for tickets selecting
+- Implemented tickets selecting 
+- Implemented Activity for draw trains info and select needed train
+- Implemented getting train list
+- Added to MainActivity Calendar for date select
+- Implemented getting station id
+- Implemented Activity with station selecting (MainActivity)
--- src/com/faustgate/ukrzaliznitsya/MainActivity.java
+++ src/com/faustgate/ukrzaliznitsya/MainActivity.java
@@ -1,6 +1,7 @@
 package com.faustgate.ukrzaliznitsya;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,60 +9,123 @@ import android.widget.*;
 
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
+import java.util.*;
 
 public class MainActivity extends Activity {
-    private Calendar date = Calendar.getInstance();
+    private Calendar myCalendar = Calendar.getInstance();
     private String stationFromId = "";
     private String stationFromName = "";
     private String stationToId = "";
     private String stationToName = "";
+    private EditText dateEditText;
+    private List<String> trainTypes = Arrays.asList("Обычный", "Интерсити");
+    private List<String> ICCarTypes = Arrays.asList("C1", "C2");
+    private List<String> usualTrainTypes = Arrays.asList("Плацкарт", "Купе", "Люкс");
+    private List<String> operationTypes = Arrays.asList("Покупка", "Бронь");
+    private List<String> ticketTypes = Arrays.asList("Обычный", "Детский", "Студенческий");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        date.add(Calendar.DAY_OF_MONTH, 1);
+        myCalendar.add(Calendar.DAY_OF_MONTH, 1);
 
         DelayAutoCompleteTextView stationFromEdit = (DelayAutoCompleteTextView) findViewById(R.id.stationFrom);
         DelayAutoCompleteTextView stationToEdit = (DelayAutoCompleteTextView) findViewById(R.id.stationTo);
-        DatePicker start_date = (DatePicker) findViewById(R.id.datePicker);
+        // DatePicker start_date = (DatePicker) findViewById(R.id.datePicker);
         Button search = (Button) findViewById(R.id.button);
+        dateEditText = (EditText) findViewById(R.id.dateEditText);
+        updateLabel();
+        Spinner trainTypesSpinner = (Spinner) findViewById(R.id.trainTypeSpinner);
+        Spinner carTypeSpinner = (Spinner) findViewById(R.id.carTypeSpinner);
+        Spinner buySpinner = (Spinner) findViewById(R.id.buySpinner);
+        Spinner ticketTypeSpinner = (Spinner) findViewById(R.id.ticketTypeSpinner);
 
-        start_date.init(date.get(Calendar.YEAR),
-                date.get(Calendar.MONTH),
-                date.get(Calendar.DAY_OF_MONTH),
-                new DatePicker.OnDateChangedListener() {
-                    @Override
-                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        date.set(year, monthOfYear, dayOfMonth);
-                    }
-                });
+        trainTypesSpinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, trainTypes));
+        buySpinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, operationTypes));
+        ticketTypeSpinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, ticketTypes));
 
-        search.setOnClickListener(new View.OnClickListener() {
+        buySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                //   if ((!stationFromId.equals("") && !stationToId.equals(""))) {
-                String data = MessageFormat.format("{0} {1} {2} {3} {4}", stationFromName, stationFromId, stationToName,
-                        stationToId, getDateString());
-                Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
-                String trainData = UZRequests.getInstance().searchForTrains(stationFromId, stationToId, date.getTime());
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    ticketTypeSpinner.setEnabled(false);
+                } else {
+                    ticketTypeSpinner.setEnabled(true);
+                }
+            }
 
-                Intent intent = new Intent(MainActivity.this, TrainListActivity.class);
-                intent.putExtra("trains", trainData);
-                startActivity(intent);
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
 
+        trainTypesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 1) {
+                    carTypeSpinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, ICCarTypes));
+                } else {
+                    carTypeSpinner.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, usualTrainTypes));
+                }
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!stationFromId.equals("") && !stationToId.equals("")) {
+                    String data = MessageFormat.format("{0} {1} {2} {3} {4}", stationFromName, stationFromId, stationToName,
+                            stationToId, getDateString());
+                    Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+                    String trainData = UZRequests.getInstance().searchForTrains(stationFromId, stationToId, myCalendar.getTime());
+                    Intent intent = new Intent(MainActivity.this, TrainListActivity.class);
+                    intent.putExtra("trains", trainData);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dateEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    new DatePickerDialog(MainActivity.this, date, myCalendar
+                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+            }
+        });
 
         //bookTitle.setThreshold(4);
         StationAutoCompleteAdapter adapter = new StationAutoCompleteAdapter(getApplicationContext());
         stationFromEdit.setAdapter(adapter);
         stationFromEdit.setLoadingIndicator((ProgressBar) findViewById(R.id.progress_bar));
-        stationFromEdit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        stationFromEdit.setOnItemClickListener(new AdapterView.OnItemClickListener()
+
+        {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 HashMap<String, String> station = (HashMap<String, String>) adapterView.getItemAtPosition(position);
@@ -73,7 +137,9 @@ public class MainActivity extends Activity {
 
         stationToEdit.setAdapter(adapter);
         stationToEdit.setLoadingIndicator((ProgressBar) findViewById(R.id.progress_bar));
-        stationToEdit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        stationToEdit.setOnItemClickListener(new AdapterView.OnItemClickListener()
+
+        {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 HashMap<String, String> station = (HashMap<String, String>) adapterView.getItemAtPosition(position);
@@ -82,12 +148,16 @@ public class MainActivity extends Activity {
                 stationToEdit.setText(stationToName);
             }
         });
-
-
     }
 
     private String getDateString() {
         SimpleDateFormat spf = new SimpleDateFormat("MM.dd.yyyy");
-        return spf.format(date.getTime());
+        return spf.format(myCalendar.getTime());
+    }
+
+    private void updateLabel() {
+        String myFormat = "EEEE, MMMM dd, yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateEditText.setText(sdf.format(myCalendar.getTime()));
     }
 }
